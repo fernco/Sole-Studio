d536207034eec0c14f0e3d60b28bd4e691891a39
Fern Coelho <123507401+fernco@users.noreply.github.com>
2023-12-04 08:35:56 -0700

    -

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,17 +1,15 @@
 const db = require('./client');
-
-// Assuming 'createUser' function is properly defined elsewhere
-const createUser = require('./users').createUser;
+const { createUser } = require('./users');
 
 const users = [
   {
     first_name: 'Emily',
-    last_name: 'Johnson',
+    last_name: 'Jonhson',
     email: 'emily@example.com',
     password: 'securepass',
   },
   {
-    first_name: 'Liu',
+    first_name: 'Lui',
     last_name: 'Wei',
     email: 'liu@example.com',
     password: 'strongpass',
@@ -22,58 +20,46 @@ const users = [
     email: 'bella@example.com',
     password: 'pass1234',
   },
+ 
   // Add more user objects as needed
 ];  
 
 const dropTables = async () => {
     try {
         await db.query(`
-        DROP TABLE IF EXISTS order_product;
-        DROP TABLE IF EXISTS orders;
-        DROP TABLE IF EXISTS products;
         DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS products;
         `);
-    } catch(err) {
-        console.error('Error dropping tables:', err);
+    }
+    catch(err) {
         throw err;
     }
 }
 
 const createTables = async () => {
-    try {
+  console.log('Dropping all tables...');
+    try{
         await db.query(`
-        CREATE TABLE users (
+        CREATE TABLE users(
             id SERIAL PRIMARY KEY,
-            first_name VARCHAR(255) NOT NULL,
-            last_name VARCHAR(255) NOT NULL,
+            first_name VARCHAR(255) DEFAULT 'first_name',
+            last_name VARCHAR(255) DEFAULT 'last_name',
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL
         );
 
-        CREATE TABLE products (
+        CREATE TABLE products(
           id SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          description TEXT,
-          price DECIMAL NOT NULL,
-          image_url TEXT
+          name VARCHAR(255),
+          description TEXT NOT NULL,
+          price DECIMAL,
+          img TEXT
         );
 
-        CREATE TABLE orders (
-          id SERIAL PRIMARY KEY,
-          user_id INTEGER REFERENCES users(id),
-          order_Date TIMESTAMP,
-          order_status VARCHAR(255) NOT NULL,
-          total DECIMAL NOT NULL
-        );
-
-        CREATE TABLE order_product (
-          order_id INTEGER REFERENCES orders(id),
-          product_id INTEGER REFERENCES products(id),
-          quantity INTEGER NOT NULL
-        );
-        `);
-    } catch(err) {
-        console.error('Error creating tables:', err);
+        
+      `)
+    } 
+    catch(err) {
         throw err;
     }
 }
@@ -81,28 +67,28 @@ const createTables = async () => {
 
 const insertUsers = async () => {
   try {
-    await Promise.all(users.map(user => 
-      createUser({
-        first_name: user.first_name, 
-        last_name: user.last_name, 
-        email: user.email, 
-        password: user.password
-      })
-    ));
+    for (const user of users) {
+      await createUser({name: user.name, email: user.email, password: user.password});
+    }
     console.log('Seed data inserted successfully.');
   } catch (error) {
     console.error('Error inserting seed data:', error);
   }
 };
 
-const seedDatabase = async () => {
+const seedDatabse = async () => {
     try {
+        db.connect();
         await dropTables();
         await createTables();
         await insertUsers();
-    } catch (err) {
-        console.error('Error seeding database:', err);
+    }
+    catch (err) {
+        throw err;
+    }
+    finally {
+        db.end()
     }
 }
 
-seedDatabase();
+seedDatabse()
