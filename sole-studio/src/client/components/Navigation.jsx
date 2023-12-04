7d987fac2ea2f04356e920001124894905e9626f
monica ran <ranmonica96@gmail.com>
2023-12-04 10:26:57 -0800

    ran vite and reorganized component files, still need to add the seed script into package.json

---
+++ sole-studio/src/client/components/Navigation.jsx
@@ -0,0 +1,17 @@
+// import { Link } from 'react-router-dom'
+// import React from 'react'
+
+// export default function Navigation() {
+//     return (
+//         <div className='navigationBar'>
+//             <h1>Sole Studio</h1>
+//             <ul id='navLinks'>
+//                 <li><Link to='/Home'>Home</Link></li>
+//                 <li><Link to='/Products'>Products</Link></li>
+//                 <li><Link to='/Design'>Design</Link></li>
+//                 <li><Link to='/About'>About</Link></li>
+                
+//             </ul>
+//         </div>
+//     );
+// }
